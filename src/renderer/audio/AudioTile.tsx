e68fa44caa567c49e7cbbf5f466d0333c041928e
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2021-09-14 18:27:05 +0200

    make playing audio by pressing buttons work
    using HTMLAudioElement for loading and playing since it can play multiple sounds simultaneously unlike audio-play

--- src/renderer/audio/AudioTile.tsx
+++ src/renderer/audio/AudioTile.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function AudioTile({ fileNames, fileIndex }: AudioTileProps) {
+    const [audio, setAudio] = useState({} as HTMLAudioElement);
+
+    useEffect(() => {
+        const loadedAudio = new Audio(fileNames[fileIndex]);
+        setAudio(loadedAudio);
+    }, [])
+
+    // TODO: Test & add playback-status (playing/paused)
     return(
-        <div>{ fileNames[fileIndex] }</div>
+        <div>
+            <button onClick={() => {audio.play(); console.log("playing " + audio)}}>{ fileNames[fileIndex] }</button>
+        </div>
     )
 }
 
