b4c9d60fca143b04ef5752b67799a84af83aa1eb
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2021-09-16 21:34:21 +0200

    add pretty & clickable progress bar

--- src/renderer/audio/AudioTile.tsx
+++ src/renderer/audio/AudioTile.tsx
@@ -2,33 +2,79 @@ import React, { useEffect, useState, useRef } from 'react';
 import AudioControls from './AudioControls';
 
 export default function AudioTile({ fileNames, fileIndex }: AudioTileProps) {
+	const [ trackProgress, setTrackProgress ] = useState(0);
 	const [ isPlaying, setIsPlaying ] = useState(false);
 
 	const audioRef = useRef(new Audio(fileNames[fileIndex]));
+	const intervalRef = useRef<NodeJS.Timeout>();
+	const isReady = useRef(false);
 
 	useEffect(
 		() => {
 			if (isPlaying) {
 				audioRef.current.play();
+				startTimer();
 			} else {
+				clearInterval(intervalRef.current!);
 				audioRef.current.pause();
 			}
 		},
 		[ isPlaying ]
 	);
 
+	useEffect(() => {
+		// Pause and clean up on unmount
+		return () => {
+			audioRef.current.pause();
+			clearInterval(intervalRef.current!);
+		};
+	}, []);
+
 	const { duration } = audioRef.current;
 	const title = fileNames[fileIndex].split('/').pop();
 
 	const reset = () => {
 		setIsPlaying(false);
+		clearInterval(intervalRef.current!);
 		audioRef.current.currentTime = 0;
+		setTrackProgress(0);
 	};
 
 	const replay = () => {
 		audioRef.current.currentTime = 0;
+		setTrackProgress(0);
 		setIsPlaying(true);
 	};
+
+	const startTimer = () => {
+		// Clear any timers already running
+		clearInterval(intervalRef.current!);
+
+		intervalRef.current = setInterval(() => {
+			setTrackProgress(audioRef.current.currentTime);
+			if (audioRef.current.currentTime == duration) {
+				reset();
+			}
+		}, 200);
+	};
+
+	const onScrub = (value: number) => {
+		// Clear any timers already running
+		clearInterval(intervalRef.current!);
+		audioRef.current.currentTime = value;
+		setTrackProgress(audioRef.current.currentTime);
+	};
+
+	const onScrubEnd = () => {
+		// If not already playing, start
+		if (!isPlaying) {
+			setIsPlaying(true);
+		}
+		startTimer();
+	};
+
+	const currentPercentage = duration ? `${trackProgress / duration * 100}%` : '0%';
+	const trackStyling = `-webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))`;
 
 	return (
 		<div className="audio-player">
@@ -44,6 +90,18 @@ export default function AudioTile({ fileNames, fileIndex }: AudioTileProps) {
 					onResetClick={reset}
 					onReplayClick={replay}
 					onPlayPauseClick={setIsPlaying}
+				/>
+				<input
+					type="range"
+					value={trackProgress}
+					step="1"
+					min="0"
+					max={duration ? duration : `${duration}`}
+					className="progress"
+					onChange={(e) => onScrub(parseInt(e.target.value))}
+					onMouseUp={onScrubEnd}
+					onKeyUp={onScrubEnd}
+					style={{ background: trackStyling }}
 				/>
 			</div>
 		</div>
